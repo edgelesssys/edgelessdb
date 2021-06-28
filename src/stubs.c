f51dbce59ecae50a4fe6ff6e20ea47ab2098ad99
Nils Hanke <nils.hanke@rub.de>
2021-06-28 12:58:03 +0200

    Fix build on Ubuntu 20.04

--- src/stubs.c
+++ src/stubs.c
@@ -1,4 +1,6 @@
+#include <fcntl.h>
 #include <openenclave/ert_stubs.h>
+#include <stdarg.h>
 
 ERT_STUB(backtrace_symbols_fd, 0)
 ERT_STUB(fedisableexcept, -1)
@@ -10,6 +12,7 @@ ERT_STUB(mallinfo, 0)
 ERT_STUB_SILENT(pthread_setname_np, 0)
 ERT_STUB(pthread_yield, -1)
 ERT_STUB(setcontext, -1)
+ERT_STUB(__fdelt_chk, -1)  // disable fortify
 
 // musl implements POSIX which returns int, but we
 // compile mariadb with glibc which returns char*
@@ -21,3 +24,15 @@ char* strerror_r(int err) {
 }
 // musl defines this in strerror_r.c. We must also do it to prevent multiple definition error.
 OE_WEAK_ALIAS(strerror_r, __xpg_strerror_r);
+
+// Redirect fcntl64 to fcntl
+int fcntl64(int fd, int cmd, ...) {
+  va_list ap;
+  void* arg;
+
+  va_start(ap, cmd);
+  arg = va_arg(ap, void*);
+  va_end(ap);
+
+  return fcntl(fd, cmd, arg);
+}
