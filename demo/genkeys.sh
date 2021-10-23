448f9969e55cc06348f6110fb3877fea47b8d949
Thomas Tendyck <51411342+thomasten@users.noreply.github.com>
2021-10-23 17:03:05 +0200

    Update genkeys.sh

--- demo/genkeys.sh
+++ demo/genkeys.sh
@@ -1,9 +1,9 @@
 set -e
 mkdir owner reader writer
-openssl req -x509 -newkey rsa -nodes -subj '/CN=Owner CA' -keyout owner/ca-key.pem -out owner/ca-cert.pem
+openssl req -x509 -newkey rsa -nodes -days 3650 -subj '/CN=Owner CA' -keyout owner/ca-key.pem -out owner/ca-cert.pem
 openssl req -newkey rsa -nodes -subj '/CN=Reader' -keyout reader/key.pem -out csr-reader.pem
-openssl x509 -req -CA owner/ca-cert.pem -CAkey owner/ca-key.pem -CAcreateserial -in csr-reader.pem -out reader/cert.pem
+openssl x509 -req -days 3650 -CA owner/ca-cert.pem -CAkey owner/ca-key.pem -CAcreateserial -in csr-reader.pem -out reader/cert.pem
 openssl req -newkey rsa -nodes -subj '/CN=Writer' -keyout writer/key.pem -out csr-writer.pem
-openssl x509 -req -CA owner/ca-cert.pem -CAkey owner/ca-key.pem -in csr-writer.pem -out writer/cert.pem
+openssl x509 -req -days 3650 -CA owner/ca-cert.pem -CAkey owner/ca-key.pem -in csr-writer.pem -out writer/cert.pem
 rm csr-reader.pem csr-writer.pem
 ./genkeys-addtomanifest.py
