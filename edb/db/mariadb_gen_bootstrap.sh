fa4919206c78b2ed6c09795fdd94ca2f780b5b98
Thomas Tendyck <tt@edgeless.systems>
2022-06-08 11:22:55 +0200

    update edgeless-mariadb to 10.6.8

--- edb/db/mariadb_gen_bootstrap.sh
+++ edb/db/mariadb_gen_bootstrap.sh
@@ -2,5 +2,5 @@ set -e
 echo 'package db'
 echo
 echo 'const mariadbBootstrap = `'
-sed 's/`/` + "`" + `/g' "$1/scripts/mysql_system_tables.sql"
+sed -e 's/`/` + "`" + `/g' -e 's/CREATE TEMPORARY TABLE tmp_/CREATE TABLE tmp_/' "$1/scripts/mysql_system_tables.sql"
 echo '`'
