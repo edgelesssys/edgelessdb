ce9c775cfe2a488b1da958bdadd2a7ef31b8dc58
Thomas Tendyck <tt@edgeless.systems>
2022-08-17 17:49:38 +0200

    prepare release

--- src/entry.sh
+++ src/entry.sh
@@ -5,9 +5,12 @@ if [ -n "${PCCS_ADDR}" ]; then
 	PCCS_URL=https://${PCCS_ADDR}/sgx/certification/v3/
 fi
 
+if [ -z "${PCCS_URL}" ] && [ "$(cat /sys/devices/virtual/dmi/id/chassis_asset_tag)" != 7783-7084-3265-9085-8269-3286-77 ]; then
+	PCCS_URL=https://172.17.0.1:8081/sgx/certification/v3/
+fi
+
 if [ -n "${PCCS_URL}" ]; then
 	apt-get install -qq libsgx-dcap-default-qpl
-	ln -fs /usr/lib/x86_64-linux-gnu/libdcap_quoteprov.so.1 /usr/lib/x86_64-linux-gnu/libdcap_quoteprov.so
 	echo "PCCS_URL: ${PCCS_URL}"
 	echo "PCCS_URL=${PCCS_URL}\nUSE_SECURE_CERT=FALSE" > /etc/sgx_default_qcnl.conf
 else
